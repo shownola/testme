57abd636da73f455b72bfaa8cd1236f0ef67f44b
shownola <swasieko@gmail.com>
2020-02-11 16:09:01 -0700

    Implement Breadth First Search

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
+const Runner = require('./runner');
+const runner = new Runner();
+
+// await runner.collectFiles();
+const run = async () => {
+  const results = await runner.collectFiles(process.cwd());
+  console.log(results);
+};
+
 console.log('Running test...');
+
+run();
